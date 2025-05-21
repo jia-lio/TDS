8bf0936f77f44e14d5d340e56376ecf0a8bb7dc9
jia <kimgw4567@gmail.com>
2025-05-21 18:01:48 +0900

    feature : 좀비쌓기

--- Assets/2.Scripts/Monster/MonsterController.cs
+++ Assets/2.Scripts/Monster/MonsterController.cs
@@ -8,18 +8,26 @@ namespace Game.Monster
         None,
         Attack,
         Die,
-        Run
+        Run,
     }
     
     public class MonsterController : MonoBehaviour
     {
         public Rigidbody2D rigidbody;
+        public BoxCollider2D collider;
         public SortingGroup groupLayer;
         
         private Transform _target;
         private EState _state = EState.None;
 
         private int _layer;
+        private float _jumpCoolTime = 1f;
+        private float _lastJumpTime = -1f;
+        private float _jumpTargetY;
+
+        private bool _isJump;
+
+        private RaycastHit2D[] _results = new RaycastHit2D[100];
         
         public void Init(Transform target, int layer)
         {
@@ -60,9 +68,24 @@ namespace Game.Monster
         {
             
         }
-
+        
         private void OnStateFixedUpdate()
         {
+            if (_isJump)
+            {
+                var currentPos = transform.position;
+                var targetPos = new Vector3(currentPos.x, _jumpTargetY, currentPos.z);
+
+                transform.position = Vector3.MoveTowards(currentPos, targetPos, 2 * Time.deltaTime);
+
+                if (Mathf.Abs(transform.position.y - _jumpTargetY) < 0.01f)
+                {
+                    _isJump = false;
+                }
+                
+                return;
+            }
+
             switch (_state)
             {
                 case EState.Run:
@@ -73,11 +96,120 @@ namespace Game.Monster
                     break;
             }
         }
-        
+
         private void FixedUpdate()
         {
             SetState();
             OnStateFixedUpdate();
+        }
+
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            if (Time.time - _lastJumpTime < _jumpCoolTime)
+                return;
+            
+            if (collision.gameObject.CompareTag("Monster"))
+            {
+                var other = collision.gameObject.GetComponent<MonsterController>();
+                if (other != null && other._layer == _layer)
+                {
+                    var myX = transform.position.x;
+                    var otherX = other.transform.position.x;
+                    var isCover = myX > otherX;
+
+                    if (isCover == false)
+                        return;
+
+                    var targetY = _target.position.y - 2f;
+                    if (transform.position.y + 1.05f > targetY)
+                        return;
+
+                    if (IsLeftHit() && IsUpHit() == false && IsUpLeftHit() == false)
+                    {
+                        _jumpTargetY = transform.position.y + 1.05f;
+                        _isJump = true;
+                        _lastJumpTime = Time.time;
+                    }
+
+                    IsLeftHit();
+                }
+            }
+        }
+        
+        private void OnDrawGizmos()
+        {
+            // 큐브 색상 지정
+            Gizmos.color = Color.magenta;
+
+            // Bounds 기준으로 WireCube 표시
+            Gizmos.DrawWireCube(collider.bounds.center, collider.bounds.size);
+            
+            Vector2 origin = collider.bounds.center;
+            Vector2 end = origin + new Vector2(-0.5f, 1);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(origin, end);
+
+        }
+        
+        private bool IsLeftHit()
+        {
+            var origin = (Vector2)collider.bounds.center;
+            var hits = Physics2D.LinecastAll(origin, origin + Vector2.left * 0.5f);
+
+            foreach (var hit in hits)
+            {
+                if (hit.collider != null && hit.collider.gameObject != gameObject)
+                {
+                    var hitMonster = hit.collider.GetComponent<MonsterController>();
+                    if (hitMonster != null && hitMonster.gameObject.layer == gameObject.layer)
+                    {
+                        return true;
+                    }
+                }
+            }
+            
+            return false;
+        }
+
+        private bool IsUpHit()
+        {
+            var origin = (Vector2)collider.bounds.center;
+            var hits = Physics2D.LinecastAll(origin, origin + Vector2.up * 0.75f);
+
+            foreach (var hit in hits)
+            {
+                if (hit.collider != null && hit.collider.gameObject != gameObject)
+                {
+                    var hitMonster = hit.collider.GetComponent<MonsterController>();
+                    if (hitMonster != null && hitMonster.gameObject.layer == gameObject.layer)
+                    {
+                        return true;
+                    }
+                }
+            }
+            
+            return false;
+        }
+
+        private bool IsUpLeftHit()
+        {
+            var origin = (Vector2)collider.bounds.center;
+            var hits = Physics2D.LinecastAll(origin, origin + new Vector2(-0.5f, 1));
+
+            foreach (var hit in hits)
+            {
+                if (hit.collider != null && hit.collider.gameObject != gameObject)
+                {
+                    var hitMonster = hit.collider.GetComponent<MonsterController>();
+                    if (hitMonster != null && hitMonster.gameObject.layer == gameObject.layer)
+                    {
+                        return true;
+                    }
+                }
+            }
+            
+            return false;
         }
     }
 }
