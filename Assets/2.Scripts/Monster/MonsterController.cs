919452c6e9c431958d8d3152cccd546335b6fdc3
jia <kimgw4567@gmail.com>
2025-05-21 14:05:16 +0900

    feature : monster state, run

--- Assets/2.Scripts/Monster/MonsterController.cs
+++ Assets/2.Scripts/Monster/MonsterController.cs
@@ -1,9 +1,60 @@
+using Cysharp.Threading.Tasks.Triggers;
 using UnityEngine;
 
 namespace Game.Monster
 {
+    public enum EState
+    {
+        None,
+        Attack,
+        Die,
+        Run
+    }
+    
     public class MonsterController : MonoBehaviour
     {
+        private Transform _target;
+        private EState _state = EState.None;
+
+        public void Init(Transform target)
+        {
+            _target = target;
+        }
+
+        private void SetState()
+        {
+            var distanceX = Mathf.Abs(_target.position.x - transform.position.x);
+            if (distanceX < 1.5f)
+            {
+                _state = EState.Attack;
+            }
+            else
+            {
+                _state = EState.Run;
+            }
+        }
+
+        private void Run()
+        {
+            float dirX = _target.position.x - transform.position.x;
+            Vector3 dir = new Vector3(dirX, 0f, 0f).normalized;
+            transform.position += dir * (2f * Time.fixedDeltaTime);
+        }
+
+        private void OnStateFixedUpdate()
+        {
+            switch (_state)
+            {
+                case EState.Run:
+                    Run();
+                    break;
+            }
+        }
         
+        private void FixedUpdate()
+        {
+            SetState();
+            OnStateFixedUpdate();
+        }
     }
 }
--- Assets/2.Scripts/Monster/MonsterSpawner.cs
+++ Assets/2.Scripts/Monster/MonsterSpawner.cs
@@ -6,6 +6,7 @@ using Random = UnityEngine.Random;
 
 public class MonsterSpawner : MonoBehaviour
 {
+    public Transform target;
     public GameObject[] spawnPosition;      // 0 : 가장 위 /  1 : 중간 / 2 : 가장 밑
 
     private PoolContainer _poolContainer;
@@ -25,14 +26,16 @@ public class MonsterSpawner : MonoBehaviour
         {
             for (int i = 0; i < MONSTER_MAX_COUNT; i++)
             {
+                var delay = Random.Range(200, 2000);
+                await UniTask.Delay(delay);
                 var randomPos = Random.Range(0, spawnPosition.Length);
                 var controller = await _poolContainer.Zombie.Rent(spawnPosition[randomPos].transform);
+                controller.Init(target);
             }
         }
-        catch (Exception e)
+        catch
         {
-            Console.WriteLine(e);
-            throw;
+            // ignored
         }
     }
 
